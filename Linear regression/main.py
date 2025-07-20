336959740d45087bdc2ae4aafec43416d509a054
dim4d <dim4dmi7rij@yandex.ru>
2025-07-20 11:10:38 +0300

    Дополнение класса fit

--- Linear regression/main.py
+++ Linear regression/main.py
@@ -3,20 +3,20 @@ import pandas as pd
 
 
 class MyLineReg:
-    def __init__(self, n_iter: int = 100, learning_rate: float = 0.1):
+    def __init__(self, weights, n_iter: int = 100, learning_rate: float = 0.1):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
 
     def __str__(self):
         return f"MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
 
+    def fit(self, X: pd.DataFrame, y: pd.Series, verbose: bool = False):
+        X.insert(0, 'x0', 1)
 
 
 def main():
-    a = MyLineReg(10, 0.05)
-    print(a)
+    myLineReg = MyLineReg(10, 0.05)
 
 
 if __name__ == "__main__":
     main()
-
